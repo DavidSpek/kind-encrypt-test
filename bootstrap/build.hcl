ff768862283adeb50bb1d050d24ec01c7d229189
DavidSpek <vanderspek.david@gmail.com>
2022-08-05 11:38:48 +0200

    test encrypt

---
+++ bootstrap/build.hcl
@@ -0,0 +1,33 @@
+metadata {
+  name = "bootstrap"
+}
+
+scaffold "terraform" {
+  path = "terraform"
+  type = "terraform"
+}
+
+scaffold "crds" {
+  path = "crds"
+  type = "crd"
+}
+
+scaffold "helm" {
+  path = "helm/bootstrap"
+  type = "helm"
+
+  preflight "update-deps" {
+    wkdir   = ""
+    target  = "Chart.yaml"
+    command = "plural"
+
+    args = [
+      "wkspace",
+      "helm-deps",
+    ]
+
+    sha     = "cdac6986a5f8cc46b392a6b3db6d945225fefa495c0b832d7a007bec2987d657"
+    retries = 0
+    verbose = false
+  }
+}
---
+++ bootstrap/terraform/main.tf
@@ -0,0 +1,6 @@
+CHARTMART-ENCRYPTEDGF·d~½‚Z†sÚò9„ÎoËÔëiœdÕ
+·¨z£¨ü_Ô©¬þ>VCÍFúÏÑù6HÅzÖ¦¡iÓò^d+Ú‡õ9ÇŠ­à÷¿[B©Ncº;¿^±y~±äó\xè˜2îžùõ¸÷97A€e%lƒÁ‰ÓÓóÎU`*UJ›*Þ=]\‹<ˆÛKdJùÄ†’7ƒ$ü2³'K]¬Wø-€ùö…­L§·¹‰Òvö$
+’=Ð\”Õß)¿•ÃáqO°¶tH E­²zubè0DïdÎF´]°óÃg}d`ìqM¼³úw2g¬B7õm5¾Èçi»öþ
+)" n
+Ú²Ÿlº(‹x\3Æ“ëëê~h>ýÂOX—@÷¨Á´!ì.E##3oSl|®üÛç/¤ØýHöb«2ú|¢©.g¿8Úú½„ô”Ö‹y±ËÑ|€k_©Ãì¬û…:Õ CMj’;Ýâ4?{æ!ýô	Õƒ^“r‹Î¹òËK˜@&ápºL=ãM¹$4ØÜ4»Ý_yžÒTé‹õjIÀr³6ŽƒÊù‹¶¯¥î¯sr7YËö~²/ŠBþ¥ŠK
+o@,„H!œ<çVÙúŒ_[ìk•ì‹jnbJíðÐ ¾5YñÄ‰½Ê®X¸.)dFß­-·|”ZÏ\&/‹Î‚Hq¼B6üCáñè¬'ì„ÅÇâß<ÕÙ!·wß_”r»&õ†	'Íß–Z$¢·b2ÿD¼œt ›¢DÚ3ºÙ7è©ˆæ¿73ÿrä”‹¶[«õ¥ÅWD*j_‘Þê9Ô£MË(ÿ“5©á‚«!•¥dX?àXü‹ƒÝ+Va"ÝÛÁî$'ÜyùØ—¬ç®Aç{D<÷žOcc¯”idž9³Æ­þß¦ˆÅ¹Õ2_ŠÀ­Aµ3Ÿ­ÁŠ3Âñ'ÑÃ´ô†OÐ\¶)Œ' áïŒW“ßôã‹'o;+ü½w.U
